bd91573499fba49c7a85c93a54b571050e38870e
lyle dickie <lyle.e.dickie@gmail.com>
2019-08-19 23:16:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,10 @@
 def reverse_each_word(string)
   ary = string.split()
   ary.each do |word|
-    word.reverse
+    word.reverse!
+  end
   
-  result = result.join(" ").reverse()
+  result = ary.join(" ")
   return result
 end
 
